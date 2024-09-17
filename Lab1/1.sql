a6f69a67a606d929b0b3b6f37cac4e666f74c98c
AidaRam6 <157144106+AidaRam6@users.noreply.github.com>
2024-09-17 15:54:42 +0600

    Update 1.sql

--- Lab1/1.sql
+++ Lab1/1.sql
@@ -1,1 +1,28 @@
 CREATE DATABASE lab1;
+\c  lab1
+CREATE table users(
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname  VARCHAR(50)
+);
+
+ALTER TABLE users
+ADD COLUMN isadmin INTEGER DEFAULT 0;
+
+ALTER TABLE users
+ALTER COLUMN isadmin TYPE BOOLEAN USING isadmin::BOOLEAN;
+
+ALTER TABLE users
+ALTER COLUMN isadmin SET DEFAULT FALSE;
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    user_id INTEGER
+);
+
+DROP TABLE tasks;
+
+\c postgres
+DROP DATABASE lab1;
+
